6ee20e7041609c54abe09b61f658d8f11cdf9bf4
juanmanuel <juanmanueltarrago@gmail.com>
2023-07-03 20:38:20 -0300

    ahora si con ssh

--- hellogit.py
+++ hellogit.py
@@ -1,4 +1,4 @@
 print("New hello git! with chages! v3")
 print("bueno este es un nuevo cambio")
 print("este es otro cambio")
-print("esta es otro prueba de ")
+print("esta es otro prueba de kljdkjkjl")

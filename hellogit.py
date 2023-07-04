f1c7a8a3da3a179c5eccf3846e45ee26f37ccbba
juanmanuel <juanmanueltarrago@gmail.com>
2023-07-04 18:43:28 -0300

    hello

--- hellogit.py
+++ hellogit.py
@@ -1,4 +1,5 @@
 print("New hello git! with chages! v3")
 print("bueno este es un nuevo cambio")
 print("este es otro cambio")
-print("esta es otro prueba de kljdkjkjl")
+print("esta es otro prueba de si kljdkjkjl")
+print("new")
